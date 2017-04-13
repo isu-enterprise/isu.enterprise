ab8a094efc3dfe541ab379b4f6830d19513e483e
Evgeny Cherkashin <eugene@irnok.net>
2017-04-13 23:06:38 +0800

    Just return configurator object.

--- src/isu/enterprise/configurator.py
+++ src/isu/enterprise/configurator.py
@@ -1,28 +1,30 @@
 from isu.enterprise.interfaces import IConfigurator
 from zope.component import getGlobalSiteManager
 import sys
-if sys.version_info.major>2:
+if sys.version_info.major > 2:
     from configparser import ConfigParser, ExtendedInterpolation
 else:
-    from ConfigParser import ConfigParser #, ExtendedInterpolation
+    from ConfigParser import ConfigParser  # , ExtendedInterpolation
 import os
+
 
 def registerConfigurator(obj, registry=None, name=""):
     if registry is None:
-        registry=getGlobalSiteManager()
+        registry = getGlobalSiteManager()
     registry.registerUtility(obj,
-        provided=IConfigurator,
-        name=name)
+                             provided=IConfigurator,
+                             name=name)
+
 
 def createConfigurator(ini, registry=None, name=""):
-    conf = ConfigParser(defaults=os.environ #,
-        #interpolation=ExtendedInterpolation()
-        )
+    conf = ConfigParser(defaults=os.environ,
+                        interpolation=ExtendedInterpolation()
+                        )
     conf.read(ini)
     registerConfigurator(conf, registry=registry, name=name)
+    return conf
+
 
 def initZCML():
-    package=__name__
+    package = __name__
     xmlconfig(resource_stream(package, "configure.zcml"))
-
-
